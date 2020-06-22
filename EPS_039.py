7e53ca60d6b28bd520cc59276bfadb5f308421c9
Jaegwan <bluewaves@daum.net>
2020-06-22 18:01:00 +0900

    Update EPS_039.py

--- EPS_039.py
+++ EPS_039.py
@@ -49,10 +49,53 @@ print(summary_df.mean())
 print(np.mean(deviation ** 2))
 # **에 다음 나온 수는 승수를 말함
 
-print(np.var(scores)
+print(np.var(scores))
 # Numpy 패키지에서는 var()를 사용
 # 판다스에도 있지만 가급적 넘파이에서 사용할 것을 권장(넘파이는 표본분산이고 판다는 불편분산임, 불편분산은 추측통계에서 역할을 함)
 # 표 데이터로 만들기 -> 판다스, 데이터의 연산 -> 넘파이, (아직 나오지 않았지만) 데이터 시각화 -> 맷플롯립
+
+
+
+print(scores_df.var())
+# 판다스에서 다루는 분산은 넘파이에서 다루는 분산과 차이가 있음
+# 판다스 -> 불편분산, 넘파이 -> 표본분산
+# var 매서드 인수 ddof=0를 이용하여 표본분산으로 계산가능 
+
+
+summary_df['square of deviation']=np.square(deviation)
+# 분산 값을 'square of deviation' 열에 저장
+print(summary_df)
+
+
+print(summary_df.mean())
+
+print(np.sqrt(np.var(scores, ddof=0)))
+# ddof=0 -> 표본분산
+
+print(np.std(scores, ddof=0))
+# 표준편차
+
+print(np.max(scores) - np.min(scores))
+# 범위(range)
+
+# 사분면 범위
+scores_Q1 = np.percentile(scores, 25)
+scores_Q3 = np.percentile(scores, 75)
+scores_IQR = scores_Q3 - scores_Q1
+print(scores_IQR)
+
+# 데이터 지표 정리 - 51페이지
+print(pd.Series(scores).describe())
+
+
+# Section 2.3 데이터의 정규화
+# 2.3.1 표준화
+
+z = (scores - np.mean(scores)) / np.std(scores)
+print(z)
+
+print(round(np.mean(z)), round(np.std(z, ddof=0)))
+
 
 
 
